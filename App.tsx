64eacb14bf3b2989878fb4cc145e70641b92c991
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-23 07:38:14 -0300

    initialization updated

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Home } from './src/screens/Home';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }
