7a882d765904a9be847941ed70606c4d5e445821
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-22 22:06:42 -0300

    setup of application

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from "react-native";
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Batatinha</Text>
+    </View>
+  )
+}
