6efd4ba93a49226c2f1a08d63f00676a1a38d1c6
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-23 07:52:24 -0300

    Home created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,17 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { Text, View } from "react-native";
 import { styles } from './styles';
 
 export function Home() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) <AppLoading />
+
   return (
     <View style={styles.container}>
       <Text>Batatinha</Text>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../theme";
 
  export const styles = StyleSheet.create({
    container: {
      flex: 1,
+     backgroundColor: COLORS.BLACK_SECONDARY,
      justifyContent: 'center',
      alignItems: 'center'
    }
