82ce4bc13600d7ec911f802c2ac7bdc461762947
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-25 20:24:04 -0300

    import fonts and add loading page on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Home } from './src/screens/Home';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) <AppLoading />
+
   return (
     <>
       <StatusBar style="light" />
