4403e53cc61892a799f22019042d89c5147fe1cd
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-25 20:31:24 -0300

    add padding top into home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,12 @@
 import { StyleSheet } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
 import { COLORS } from "../../theme";
 
  export const styles = StyleSheet.create({
    container: {
      flex: 1,
-     backgroundColor: COLORS.BLACK_SECONDARY
+     backgroundColor: COLORS.BLACK_SECONDARY,
+     paddingTop: getStatusBarHeight() + 16
    }
  })
