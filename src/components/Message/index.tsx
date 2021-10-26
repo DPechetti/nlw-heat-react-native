a395d9cddfcbcb1e7c44d5d39f888edbf9e96382
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-26 12:44:47 -0300

    Message component created

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+import { UserPhoto } from '../UserPhoto';
+
+import { styles } from './styles';
+
+export function Message(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.message}>Texto da batatinha</Text>
+
+      <View style={styles.footer}>
+        <UserPhoto sizes='SMALL' imageUri='https://github.com/dpechetti.png' />
+
+      <Text style={styles.userName}>Nome da batatinha</Text>
+      </View>
+    </View>
+  );
+}
