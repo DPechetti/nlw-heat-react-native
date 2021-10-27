13d24fa8d768fa23aa27c7d2b44c894aa7a3a511
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-26 21:31:16 -0300

    button component created

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { FONTS } from '../../theme';
+
+export const styles = StyleSheet.create({
+  button: {
+    height: 48,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  icon: {
+    marginRight: 12
+  },
+
+  title: {
+    fontSize: 14,
+    fontFamily: FONTS.BOLD
+  }
+});
