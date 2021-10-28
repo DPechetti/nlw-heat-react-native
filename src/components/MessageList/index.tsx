f078fd75d0ccde094c7358233009a0425fac11a6
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-28 08:25:42 -0300

    add socket and real time refresh

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import socketClient from 'socket.io-client';
 import { ScrollView } from 'react-native';
-import { api } from '../../services/api';
 
 import { Message, MessageProps } from '../Message';
+import { api } from '../../services/api';
 
 import { styles } from './styles';
+
+const messagesQueue: MessageProps[] = []
+
+const socket = socketClient(String(api.defaults.baseURL))
+
+socket.on('newMessage', (newMessage: MessageProps) => messagesQueue.push(newMessage))
 
 export function MessageList(){
   const [messages, setMessages] = useState<MessageProps[]>([])
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) 
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+    }, 3000)
+
+    return () => clearInterval(timer)
+  }, [])
+  
   useEffect(() => {
     async function fetchMessages() {
       const messagesResponse = await api.get<MessageProps[]>('/messages/last3')
