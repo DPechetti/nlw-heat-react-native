550efb559d4a0d4cf2d1c6e6f3410e669be71408
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-28 08:13:00 -0300

    send messages created

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import { TextInput, View } from 'react-native';
+import { Alert, Keyboard, TextInput, View } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import { Button } from '../Button';
 
@@ -9,6 +10,26 @@ import { styles } from './styles';
 export function SendMessageForm() {
   const [message, setMessage] = useState('')
   const [sendingMessage, setSendingMessage] = useState(false)
+
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim()
+    
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true)
+      
+      await api.post('/messages', { message: messageFormatted })
+  
+      setMessage('')
+      
+      Keyboard.dismiss()
+      Alert.alert('Mensagem enviada com sucesso!')
+      
+      setSendingMessage(false)
+    } else {
+      Alert.alert('Escreva a mensagem para enviar.')
+    }
+
+  }
 
   return (
     <View style={styles.container}>
@@ -23,7 +44,13 @@ export function SendMessageForm() {
         editable={!sendingMessage}
         style={styles.input} 
       />
-      <Button title="ENVIAR MENSAGEM" backgroundColor={COLORS.PINK} color={COLORS.WHITE} />
+      <Button 
+        title="ENVIAR MENSAGEM" 
+        backgroundColor={COLORS.PINK} 
+        color={COLORS.WHITE} 
+        isLoading={sendingMessage}
+        onPress={handleMessageSubmit}
+      />
     </View>
   );
 }
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -64,7 +64,7 @@ function AuthProvider({ children }: AuthProviderProps) {
         api.defaults.headers.common.authorization = `Bearer ${token}`
 
         await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
-        await AsyncStorage.setItem(TOKEN_STORAGE, JSON.stringify(token))
+        await AsyncStorage.setItem(TOKEN_STORAGE, token)
 
         setUser(user)
       }
